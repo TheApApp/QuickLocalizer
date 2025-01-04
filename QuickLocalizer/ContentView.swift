ad5bd06f01e4755b4b8481298907fb219c709b1a
Michael Rowe <support@theapapp.com>
2025-01-04 12:08:49 -0500

    Initial Commit

---
+++ QuickLocalizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickLocalizer
+//
+//  Created by Michael Rowe1 on 1/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickLocalizer/QuickLocalizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickLocalizerApp.swift
+//  QuickLocalizer
+//
+//  Created by Michael Rowe1 on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickLocalizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
